734f6e8afcafe302d498ec701164e6acf790614f
Hayley K <mrs.kornilenko@gmail.com>
2018-12-01 01:16:04 -0500

    updating main page

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-// import Jumbotron from '../../components/Jumbotron';
+import { Container, Row, Col, Button } from 'reactstrap';
+import Hero from '../components/Hero/Hero';
+import ytVideo from '../components/Videos/Videos';
+
+
+
 class Main extends Component {
+  state = {
+    background: "url(https://res.cloudinary.com/mrs-k/image/upload/c_scale,w_2021/v1543539825/coins.jpg) fixed"
+  }
+  
+
   render() {
-    return <h1> this worked</h1>;
+    return <Hero title="Goodbye Debt, Hello Fun" para="We all want to live for more than just paying bills and dying." background={this.state.background} size={this.state.backgroundSize}/>;
+    <Container>
+      <Row> 
+        <Col> 
+          <ytVideo />
+        </Col>
+      </Row>
+    </Container>
   }
 }
 
